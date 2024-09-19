a0d90a64355a86c53f584651f40cb2e033b1a89a
joaomorito <joaomorito04@gmail.com>
2024-09-19 14:11:04 -0300

    Payment, one-to-one association

--- src/main/java/com/projetojavaweb/course/config/TestConfig.java
+++ src/main/java/com/projetojavaweb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetojavaweb.course.entities.Category;
 import com.projetojavaweb.course.entities.Order;
 import com.projetojavaweb.course.entities.OrderItem;
+import com.projetojavaweb.course.entities.Payment;
 import com.projetojavaweb.course.entities.Product;
 import com.projetojavaweb.course.entities.User;
 import com.projetojavaweb.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
